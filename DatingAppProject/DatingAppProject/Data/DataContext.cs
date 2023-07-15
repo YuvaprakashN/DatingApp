b5a561673728b06bc84ede2f13cf45f10549c092
N <839827@cognizant.com>
2023-07-15 15:14:48 -0400

    End of section2

---
+++ DatingAppProject/DatingAppProject/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DatingAppProject.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppProject.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options):base(options) { 
+        
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
