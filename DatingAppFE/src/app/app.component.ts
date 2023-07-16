8dd736ca4bbec03ff469db2632015b72b59b135e
N <839827@cognizant.com>
2023-07-16 06:35:07 -0400

    23.Making Http Request

--- DatingAppFE/src/app/app.component.ts
+++ DatingAppFE/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  host:string="http://localhost:5125/";
   title = 'dating-app-fe';
+
+  users:any;
+  constructor(private http:HttpClient){}
+  ngOnInit(): void {
+    
+    this.http.get(this.host+"/api/users").subscribe({
+      next:res=>{this.users=res;console.log(this.users);
+      },
+      error:err=>console.log(err),
+      complete:()=>console.log("Req complted")
+    })
+    
+  }
+
+
+
 }
