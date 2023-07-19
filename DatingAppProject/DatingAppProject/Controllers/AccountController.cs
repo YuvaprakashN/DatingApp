ecbd046fd16489e22addebcc99164dc0a0398812
Yuvaprakash N <Yuvaprakashnv1999@gmail.com>
2023-07-18 22:08:30 -0400

    adding AccountController and UserDTO

---
+++ DatingAppProject/DatingAppProject/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using DatingAppProject.Data;
+using DatingAppProject.DTOs;
+using DatingAppProject.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DatingAppProject.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+
+            _context = context;
+            
+        }
+
+        [HttpPost("register")] // POST: api/account/register?username=dave&password=pwd
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.UserNAme)) return BadRequest("Username is taken");
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDto.UserNAme.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+      /*  private async Task<bool> (string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }*/
+
+        private async Task<bool> UserExists (String userName)
+    {
+            return await _context.Users.AnyAsync(u => u.UserName == userName.ToLower());
+    }
+
+    
+}
---
+++ DatingAppProject/DatingAppProject/DTOs/RegisterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DatingAppProject.DTOs
+{
+    public class RegisterDto
+    {
+        public string UserNAme { get; set; }
+        public string Password { get; set; }
+    }
+}
