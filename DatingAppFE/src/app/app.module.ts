fcf70b386de7659f0ec2b33d7390c879a6d5324b
Yuvaprakash N <Yuvaprakashnv1999@gmail.com>
2023-07-22 08:16:01 -0400

    before updating Angular CLI to v16

--- DatingAppFE/src/app/app.module.ts
+++ DatingAppFE/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import{HttpClientModule} from'@angular/common/http'
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -12,10 +17,12 @@ import { FormsModule } from '@angular/forms';
     NavComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
