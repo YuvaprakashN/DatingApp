9a2a2d1f93155daef3694b658475a3e5497855bb
Yuvaprakash N (Contractor) <Yuvaprakash.N@extcom.com>
2023-08-06 14:01:10 -0400

    using automapper

--- DatingAppProject/DatingAppProject/Controllers/UserController.cs
+++ DatingAppProject/DatingAppProject/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using DatingAppProject.Data;
+﻿using AutoMapper;
+using DatingAppProject.Data;
+using DatingAppProject.DTOs;
 using DatingAppProject.Entities;
 using DatingAppProject.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -13,25 +15,31 @@ namespace DatingAppProject.Controllers
     public class UsersController : BaseApiController
     {
         private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository,IMapper mapper)
         {
             _userRepository = userRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            return Ok( await _userRepository.GetMembersAsync());
+            var users = await _userRepository.GetMembersAsync();
+            var userToReturn=_mapper.Map<IEnumerable<MemberDto>>(users);
+
+            return Ok(userToReturn);
 
         }
 
         [HttpGet("{username}")]
 
-        public async Task<ActionResult<AppUser>> GetUser(string username)
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return await _userRepository.GetMemberAsync(username);
+            var user =await _userRepository.GetMemberAsync(username);
+            return  _mapper.Map<MemberDto>(user);
         }
     }
 }
