0455b5c4408926a9ba0412664cd9ee75e6af8932
Yuvaprakash N (Contractor) <Yuvaprakash.N@extcom.com>
2023-08-12 16:19:37 -0400

    Persisting the changes in the API

--- DatingAppProject/DatingAppProject/Controllers/UserController.cs
+++ DatingAppProject/DatingAppProject/Controllers/UserController.cs
@@ -6,6 +6,7 @@ using DatingAppProject.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace DatingAppProject.Controllers
 {
@@ -40,6 +41,21 @@ namespace DatingAppProject.Controllers
         {
             //var user =await _userRepository.GetUserByUsernameAsync(username);
             return await _userRepository.GetMemberAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
