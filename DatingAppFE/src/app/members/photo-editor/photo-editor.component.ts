a4e5cb7c2798802421daeb0c2879f162fcbbfc0c
Yuvaprakash N (Contractor) <Yuvaprakash.N@extcom.com>
2023-08-16 07:14:08 -0400

    Adding a photo uploader

--- DatingAppFE/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingAppFE/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { take } from 'rxjs';
 import { Member } from 'src/app/_models/Member';
+import { User } from 'src/app/_models/User';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-photo-editor',
@@ -8,6 +14,48 @@ import { Member } from 'src/app/_models/Member';
 })
 export class PhotoEditorComponent {
 
-  @Input() member:Member|undefined;
+  @Input() member: Member | undefined;
+  uploader: FileUploader | undefined;
+  hasBaseDropzoneOver = false;
+  baseUrl = environment.apiUrl;
+  user: User | undefined;
 
+  constructor(private accountService: AccountService, private memberService: MembersService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => {
+        if (user) this.user = user
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  fileOverBase(e: any) {
+    this.hasBaseDropzoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/add-photo',
+      authToken: 'Bearer ' + this.user?.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    }
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.member?.photos.push(photo);
+      }
+    }
+  }
 }
