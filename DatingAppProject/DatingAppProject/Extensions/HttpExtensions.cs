f597c1a990a81ff8e6168b1c441f88490a464522
Yuvaprakash N (Contractor) <Yuvaprakash.N@extcom.com>
2023-08-27 22:29:59 -0400

    Adding helper classes for pagination

---
+++ DatingAppProject/DatingAppProject/Extensions/HttpExtensions.cs
@@ -0,0 +1,17 @@
+﻿using DatingAppProject.Helpers;
+using System.Text.Json;
+
+namespace DatingAppProject.Extensions
+{
+    public static class HttpExtensions
+    {
+
+        public static void AddPaginationHeader(this HttpResponse response, PaginationHeader header)
+        {
+            var jsonOpt = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            response.Headers.Add("Pagination",JsonSerializer.Serialize(header,jsonOpt));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+
+        }
+    }
+}
