04966886ab3b27f9900570946cb2d8c72dadf5f2
Yuvaprakash N (Contractor) <Yuvaprakash.N@extcom.com>
2023-09-19 03:20:11 -0400

    Adding Action filter

--- DatingAppProject/DatingAppProject/Controllers/BaseApiController.cs
+++ DatingAppProject/DatingAppProject/Controllers/BaseApiController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DatingAppProject.Helpers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DatingAppProject.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
---
+++ DatingAppProject/DatingAppProject/Helpers/LogUserActivity.cs
@@ -0,0 +1,23 @@
+﻿using DatingAppProject.Extensions;
+using DatingAppProject.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DatingAppProject.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            ActionExecutedContext actionExecutedContext = await next();
+
+            if (!actionExecutedContext.HttpContext.User.Identity.IsAuthenticated) return;
+
+            var userName= actionExecutedContext.HttpContext.User.GetUsername();
+            var repo = actionExecutedContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
+            var user = await repo.GetUserByUsernameAsync(userName);
+            user.LastActive = DateTime.UtcNow;
+            await repo.SaveAllAsync();
+
+        }
+    }
+}
