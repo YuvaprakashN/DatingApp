3510141b2d71b66ca762e3af56dfc5f2da7cf7ac
Yuvaprakash N (Contractor) <Yuvaprakash.N@extcom.com>
2023-09-25 02:43:59 -0400

    Setting Message entity and repository

---
+++ DatingAppProject/DatingAppProject/Data/MessageRepository.cs
@@ -0,0 +1,51 @@
+﻿using DatingAppProject.DTOs;
+using DatingAppProject.Entities;
+using DatingAppProject.Helpers;
+using DatingAppProject.Interfaces;
+
+namespace DatingAppProject.Data
+{
+    public class MessageRepository : IMessageRepository
+
+    {
+
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public MessageRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        void IMessageRepository.AddMessage(Message message)
+        {
+            _context.Messages.Add(message);
+        }
+
+        void IMessageRepository.DeleteMessage(Message message)
+        {
+            _context.Messages.Remove(message);
+        }
+
+        Task<Message> IMessageRepository.GetMessage(int id)
+        {
+            return await _context.Messages.FindAsync(id);
+        }
+
+        Task<PagedList<MessageDto>> IMessageRepository.GetMessagesForUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<MessageDto>> IMessageRepository.GetMessageThread(int currentUserId, int recipientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IMessageRepository.SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ DatingAppProject/DatingAppProject/Interfaces/IMessageRepository.cs
@@ -0,0 +1,16 @@
+﻿using DatingAppProject.DTOs;
+using DatingAppProject.Entities;
+using DatingAppProject.Helpers;
+
+namespace DatingAppProject.Interfaces
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+        void DeleteMessage(Message message);
+        Task<Message> GetMessage(int id);
+        Task<PagedList<MessageDto>> GetMessagesForUser();
+        Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId,int recipientId);
+        Task<bool> SaveAllAsync();
+    }
+}
